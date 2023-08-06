80b2bdcb14889567eb5f7dadd9d3f3acac1b14c8
huangken8511429 <104516884+huangken8511429@users.noreply.github.com>
2023-08-06 15:59:41 +0800

    adjust DiseaseHandler logic

--- C4M1H1_FacadePattern/PrescriberSystem/src/Base/DiseaseHandler.java
+++ C4M1H1_FacadePattern/PrescriberSystem/src/Base/DiseaseHandler.java
@@ -1,13 +1,10 @@
 package Base;
 
 public abstract class DiseaseHandler {
-    private DiseaseHandler next;
 
     protected Prescription handle(Patient patient, Symptom[] symptoms) {
         if (matched(patient,symptoms)){
             return doHandle();
-        } else if (next != null) {
-            return next.handle(patient, symptoms);
         }
         return null;
     }
--- C4M1H1_FacadePattern/PrescriberSystem/src/Base/Main.java
+++ C4M1H1_FacadePattern/PrescriberSystem/src/Base/Main.java
@@ -4,8 +4,8 @@ public class Main {
     public static void main(String[] args) {
         PatientSystemFacade patientSystemFacade = new PatientSystemFacade();
 
-        patientSystemFacade.importPatientData("src/File/patientData.json");
-        patientSystemFacade.importSupportDiseases("src/File/supportDiseases");
+        patientSystemFacade.importPatientData("PrescriberSystem/src/File/patientData.json");
+        patientSystemFacade.importSupportDiseases("PrescriberSystem/src/File/supportDiseases");
 
         patientSystemFacade.prescribe("A123456789",new Symptom[]{Symptom.SQUEEZE,Symptom.HEADACHE,Symptom.COUGH},"Wang Xiao Ming","csv");
         patientSystemFacade.prescribe("B222312312",new Symptom[]{Symptom.SQUEEZE},"Xiao Hua","json");
